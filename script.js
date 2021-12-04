19d58b0e49c63fa6834958839638bf96c9bca809
Mohammed Nabeel <mohdnabeel13@gmail.com>
2021-12-04 16:06:26 +0000

    JS done!

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// add variables here
+let drink;
+let price;
 
 
 function checkout(choice) {
@@ -6,7 +7,16 @@ function checkout(choice) {
   document.querySelector("#checkout-img").src = `${choice}.jpg`; //look for other concatenation
 
   // add the conditional below
-  
+  if (choice === "latte") {
+    drink = "Latte";
+    price = 4.29;
+  } else if (choice === "cappuccino") {
+      drink = "Cappuccino";
+      price = 4.79;
+  } else if (choice ==="iced-coffee") {
+      drink = "Iced Coffee";
+      price = 3.79;
+  }
 
   document.querySelector("#selected-drink").innerText = drink;
   document.querySelector("#price").innerText = "$" + price.toFixed(2);
@@ -19,7 +29,7 @@ function receipt() {
   let quantity = parseInt(document.querySelector("#quantity1").value);
 
   // add quantity and total calculation below!
-  
+  let total = price * quantity;
 
   document.querySelector("#drink").innerText = drink;
   document.querySelector("#total").innerText = "$" + total.toFixed(2);
